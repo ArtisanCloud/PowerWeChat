4eeffb74e14586f701922821cdf8f0300f42620e
Matrix-X <matrix-x@artisancloud.com>
2021-06-04 18:03:26 +0800

    update

--- src/kernel/accessToken.go
+++ src/kernel/accessToken.go
@@ -1,1 +1,11 @@
 package kernel
+
+type AccessTokenInterface interface {
+	GetToken() string
+	Refresh() AccessTokenInterface
+	
+}
+
+type AccessToken struct {
+
+}
--- src/work/auth/accessToken.go
+++ src/work/auth/accessToken.go
@@ -1,1 +1,17 @@
 package auth
+
+import (
+	"github.com/ArtisanCloud/go-libs/object"
+	"github.com/ArtisanCloud/go-wechat/src/kernel"
+)
+
+type AccessToken struct {
+	BaseAccessToken kernel.AccessToken
+}
+
+func (token *AccessToken)GetCredentials() object.StringMap {
+	return object.StringMap{
+		"corpid": ,
+		"corpsecret": ,
+	}
+}
