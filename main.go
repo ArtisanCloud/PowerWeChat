da066a8851562f46810962cf725b2489b9f29f58
Matrix-X <matrix-x@artisancloud.com>
2021-06-08 18:06:27 +0800

    update

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	fmt2 "github.com/ArtisanCloud/go-libs/fmt"
 	"github.com/ArtisanCloud/go-wechat/src/work"
-	"github.com/ArtisanCloud/go-wechat/src/work/base"
 )
 
 func main() {
@@ -18,13 +17,13 @@ func main() {
 	//components := app.GetComponents()
 	//token := (*app.Components)["access_token"].(*auth.AccessToken).GetToken()
 	//fmt2.Dump(token)
-	//fmt2.Dump(app.GetConfig())
+	fmt2.Dump(app.GetConfig())
 
 	//cType := reflect.TypeOf((*app.Components)["base"].(*base.Client))
 	//fmt.Printf("kind %s \n", cType.Kind())
 	//fmt.Printf("type %v \n", cType)
 
-	ips :=(*app.Components)["base"].(*base.Client).GetCallbackIp()
-	fmt2.Dump(ips)
+	//ips :=(*app.Components)["base"].(*base.Client).GetCallbackIp()
+	//fmt2.Dump(ips)
 
 }
