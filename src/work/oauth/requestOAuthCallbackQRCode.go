59adf3d0b3d23b469cbde285b28504c69977243d
Matrix-X <matrix-x@aritsancloud.com>
2021-08-03 23:09:56 +0800

    update

--- src/work/oauth/requestOAuthCallbackQRCode.go
+++ src/work/oauth/requestOAuthCallbackQRCode.go
@@ -7,7 +7,7 @@ import (
 
 type ParaOAuthCallbackQRCode struct {
 	Code string `form:"code" json:"code" xml:"code" binding:"required"`
-	AppID string `form:"appid" json:"appid" xml:"appid" binding:"required"`
+	AppID string `form:"appid" json:"appid" xml:"appid"`
 	State string `form:"state" json:"state" xml:"state"`
 }
 
