3362bbde160d92793aaa9af4e4f11d7641904dfd
Matrix-X <hbj418@gmail.com>
2021-09-14 15:00:47 +0800

    feat(payment): split unfiy pay transcation into jssdk app native h5
    * {payment} - split unfiy pay transcation into jssdk app native h5 ...
    
    * update
    
    * update
    
    * update
    
    Co-authored-by: Matrix-X <matrix-x@artisan-cloud.com>

--- src/payment/kernel/baseClient.go
+++ src/payment/kernel/baseClient.go
@@ -1,11 +1,19 @@
 package kernel
 
 import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	fmt2 "github.com/ArtisanCloud/go-libs/fmt"
 	"github.com/ArtisanCloud/go-libs/http/request"
 	"github.com/ArtisanCloud/go-libs/http/response"
 	"github.com/ArtisanCloud/go-libs/object"
+	"github.com/ArtisanCloud/power-wechat/src/kernel"
 	"github.com/ArtisanCloud/power-wechat/src/kernel/support"
+	"io"
+	"log"
 	http2 "net/http"
+	"os"
 )
 
 type BaseClient struct {
@@ -109,6 +117,124 @@ func (client *BaseClient) Request(endpoint string, params *object.StringMap, met
 ) (response interface{}, err error) {
 
 	config := (*client.App).GetConfig()
+
+	options, err = client.AuthSignRequest(config, endpoint, method, params, options)
+	if err != nil {
+		return nil, err
+	}
+	// to be setup middleware here
+	//client.PushMiddleware(client.logMiddleware(), "access_token")
+
+	// http client request
+	returnResponse, err := client.PerformRequest(endpoint, method, options, returnRaw, outHeader, outBody)
+	if err != nil {
+		return nil, err
+	}
+
+	if returnRaw {
+		return returnResponse, nil
+	} else {
+		responseType := config.GetString("response_type", "array")
+		var rs http2.Response = http2.Response{
+			StatusCode: 200,
+			Header:     nil,
+		}
+		rs.Body = returnResponse.GetBody()
+		result, _ := client.CastResponseToType(&rs, responseType)
+		return result, nil
+	}
+
+}
+
+func (client *BaseClient) RequestRaw(url string, params *object.StringMap, method string, options *object.HashMap, outHeader interface{}, outBody interface{}) (interface{}, error) {
+	return client.Request(url, params, method, options, true, outHeader, outBody)
+}
+
+func (client *BaseClient) StreamDownload(requestDownload *response.RequestDownload, filePath string) (int64, error) {
+	fileHandler, err := os.Create(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer fileHandler.Close()
+
+	config := (*client.App).GetConfig()
+
+	method := "GET"
+	options, err := client.AuthSignRequest(config, requestDownload.DownloadURL, method, nil, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = client.PerformRequest(requestDownload.DownloadURL, method, options, true, nil, fileHandler)
+	if err != nil {
+		return 0, err
+	}
+
+	// 校验下载文件
+	downloadedHandler,err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer downloadedHandler.Close()
+
+	fileMd5 := sha1.New()
+	totalSize, err := io.Copy(fileMd5, downloadedHandler)
+	if err != nil {
+		return 0, err
+	}
+
+	//fmt2.Dump(totalSize)
+
+	if requestDownload.HashValue != "" {
+		fmt2.Dump(fileMd5.Sum(nil), requestDownload.HashValue)
+		if fmt.Sprintf("%x", fileMd5.Sum(nil)) != requestDownload.HashValue {
+			return 0, errors.New("文件损坏")
+		} else {
+			log.Println("文件SHA-256校验成功")
+		}
+	}
+
+	return totalSize, err
+}
+
+func (client *BaseClient) RequestArray(url string, method string, options *object.HashMap, outHeader interface{}, outBody interface{}) (*object.HashMap, error) {
+	returnResponse, err := client.RequestRaw(url, nil, method, options, outHeader, outBody)
+	if err != nil {
+		return nil, err
+	}
+	result, err := client.CastResponseToType(returnResponse.(*http2.Response), "array")
+
+	return result.(*object.HashMap), err
+}
+
+func (client *BaseClient) SafeRequest(url string, params *object.StringMap, method string, option *object.HashMap, outHeader interface{}, outBody interface{}) (interface{}, error) {
+	config := (*client.App).GetConfig()
+
+	return client.Request(
+		url,
+		params,
+		method,
+		&object.HashMap{
+			"cert":    config.GetString("cert_path", ""),
+			"ssl_key": config.GetString("key_path", ""),
+		},
+		false,
+		outHeader,
+		outBody,
+	)
+}
+func (client *BaseClient) Wrap(endpoint string) string {
+	if (*client.App).InSandbox() {
+		return "sandboxnew/" + endpoint
+	} else {
+		return endpoint
+	}
+}
+
+func (client *BaseClient) AuthSignRequest(config *kernel.Config, endpoint string, method string, params *object.StringMap, options *object.HashMap) (*object.HashMap, error) {
+
+	var err error
+
 	base := &object.HashMap{
 		"appid": config.GetString("app_id", ""),
 		"mchid": config.GetString("mch_id", ""),
@@ -156,66 +282,7 @@ func (client *BaseClient) Request(endpoint string, params *object.StringMap, met
 		"body": signBody,
 	}, options)
 
-	// to be setup middleware here
-	//client.PushMiddleware(client.logMiddleware(), "access_token")
-
-	// http client request
-	returnResponse, err := client.PerformRequest(endpoint, method, options, returnRaw, outHeader, outBody)
-	if err != nil {
-		return nil, err
-	}
-
-	if returnRaw {
-		return returnResponse, nil
-	} else {
-		responseType := config.GetString("response_type", "array")
-		var rs http2.Response = http2.Response{
-			StatusCode: 200,
-			Header:     nil,
-		}
-		rs.Body = returnResponse.GetBody()
-		result, _ := client.CastResponseToType(&rs, responseType)
-		return result, nil
-	}
-
-}
-
-func (client *BaseClient) RequestRaw(url string, params *object.StringMap, method string, options *object.HashMap, outHeader interface{}, outBody interface{}) (interface{}, error) {
-	return client.Request(url, params, method, options, true, outHeader, outBody)
-}
-
-func (client *BaseClient) RequestArray(url string, method string, options *object.HashMap, outHeader interface{}, outBody interface{}) (*object.HashMap, error) {
-	returnResponse, err := client.RequestRaw(url, nil, method, options, outHeader, outBody)
-	if err != nil {
-		return nil, err
-	}
-	result, err := client.CastResponseToType(returnResponse.(*http2.Response), "array")
-
-	return result.(*object.HashMap), err
-}
-
-func (client *BaseClient) SafeRequest(url string, params *object.StringMap, method string, option *object.HashMap, outHeader interface{}, outBody interface{}) (interface{}, error) {
-	config := (*client.App).GetConfig()
-
-	return client.Request(
-		url,
-		params,
-		method,
-		&object.HashMap{
-			"cert":    config.GetString("cert_path", ""),
-			"ssl_key": config.GetString("key_path", ""),
-		},
-		false,
-		outHeader,
-		outBody,
-	)
-}
-func (client *BaseClient) Wrap(endpoint string) string {
-	if (*client.App).InSandbox() {
-		return "sandboxnew/" + endpoint
-	} else {
-		return endpoint
-	}
+	return options, err
 }
 
 // ----------------------------------------------------------------------
