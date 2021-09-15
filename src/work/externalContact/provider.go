2aaea1a5624689a684788d9b638fa1472bcfbce9
Matrix-X <matrix-x@artisan-cloud.com>
2021-09-15 15:30:34 +0800

    update

--- src/work/externalContact/provider.go
+++ src/work/externalContact/provider.go
@@ -2,44 +2,55 @@ package externalContact
 
 import (
 	"github.com/ArtisanCloud/power-wechat/src/kernel"
+	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/transfer"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/contactWay"
+	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/customerStrategy"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/groupChat"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/message"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/messageTemplate"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/moment"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/school"
 	"github.com/ArtisanCloud/power-wechat/src/work/externalContact/statistics"
+	"github.com/ArtisanCloud/power-wechat/src/work/user/tag"
 )
 
 func RegisterProvider(app kernel.ApplicationInterface) (
 	*Client,
 	*contactWay.Client,
-	*statistics.Client,
-	*message.Client,
-	*school.Client,
-	*moment.Client,
-	*messageTemplate.Client,
+	*customerStrategy.Client,
 	*groupChat.Client,
+	*message.Client,
+	*messageTemplate.Client,
+	*moment.Client,
+	*school.Client,
+	*statistics.Client,
+	*tag.Client,
+	*transfer.Client,
 ) {
 	//config := app.GetConfig()
 
-	client := NewClient(app)
+	Client := NewClient(app)
+	ContactWayClient := contactWay.NewClient(app)
+	CustomerStrategy := customerStrategy.NewClient(app)
+	GroupChat := groupChat.NewClient(app)
+	Message := message.NewClient(app)
+	MessageTemplate := messageTemplate.NewClient(app)
+	Moment := moment.NewClient(app)
+	School := school.NewClient(app)
+	Statistics := statistics.NewClient(app)
+	Tag := tag.NewClient(app)
+	Transfer := transfer.NewClient(app)
 
-	contactWayClient := contactWay.NewClient(app)
+	return Client,
+		ContactWayClient,
+		CustomerStrategy,
+		GroupChat,
+		Message,
+		MessageTemplate,
+		Moment,
+		School,
+		Statistics,
+		Tag,
+		Transfer
 
-	statistics := statistics.NewClient(app)
-	message := message.NewClient(app)
-	school := school.NewClient(app)
-	moment := moment.NewClient(app)
-	messageTemplate := messageTemplate.NewClient(app)
-	groupChat := groupChat.NewClient(app)
-
-	return client,
-		contactWayClient,
-		statistics,
-		message,
-		school,
-		moment,
-		messageTemplate,
-		groupChat
 }
