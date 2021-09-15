364697705aa42d5fee156927aeff5ebbe60e1af4
Matrix-X <matrix-x@artisan-cloud.com>
2021-09-15 17:53:44 +0800

    feat(work) - add account service clients

--- src/work/accountService/customer/client.go
+++ src/work/accountService/customer/client.go
@@ -3,6 +3,8 @@ package customer
 import (
 	"github.com/ArtisanCloud/go-libs/object"
 	"github.com/ArtisanCloud/power-wechat/src/kernel"
+	response2 "github.com/ArtisanCloud/power-wechat/src/kernel/response"
+	"github.com/ArtisanCloud/power-wechat/src/work/accountService/customer/request"
 	"github.com/ArtisanCloud/power-wechat/src/work/accountService/customer/response"
 )
 
@@ -31,17 +33,24 @@ func (comp *Client) BatchGet(externalUserIDList []string) (*response.ResponseCus
 	return result, err
 }
 
-
-
 // 获取配置的专员与客户群
 // https://work.weixin.qq.com/api/doc/90000/90135/94674
 func (comp *Client) GetUpgradeServiceConfig() (*response.ResponseCustomerGetUpgradeServiceConfig, error) {
 
 	result := &response.ResponseCustomerGetUpgradeServiceConfig{}
 
-
 	_, err := comp.HttpGet("cgi-bin/kf/customer/get_upgrade_service_config", nil, nil, result)
 
 	return result, err
 }
 
+// 为客户升级为专员或客户群服务
+// https://work.weixin.qq.com/api/doc/90000/90135/94674
+func (comp *Client) UpgradeService(options request.RequestUpgradeService) (*response2.ResponseWork, error) {
+
+	result := &response2.ResponseWork{}
+
+	_, err := comp.HttpPostJson("cgi-bin/kf/customer/upgrade_service", options, nil, nil, result)
+
+	return result, err
+}
