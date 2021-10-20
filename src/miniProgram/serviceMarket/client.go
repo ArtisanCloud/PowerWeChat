b4485ef80d3279d473ab6332c6d6296105320c66
Walle <walle@artisan-cloud.com>
2021-10-20 11:02:50 +0800

    feat(miniprogram): add customer service, risk control, service markert struct

--- src/miniProgram/serviceMarket/client.go
+++ src/miniProgram/serviceMarket/client.go
@@ -1,9 +1,8 @@
 package serviceMarket
 
 import (
-	"github.com/ArtisanCloud/PowerLibs/object"
 	"github.com/ArtisanCloud/PowerWeChat/src/kernel"
-	"github.com/ArtisanCloud/PowerWeChat/src/kernel/power"
+	"github.com/ArtisanCloud/PowerWeChat/src/miniProgram/serviceMarket/request"
 	"github.com/ArtisanCloud/PowerWeChat/src/miniProgram/serviceMarket/response"
 )
 
@@ -13,18 +12,11 @@ type Client struct {
 
 // 调用服务平台提供的服务
 // https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/service-market/serviceMarket.invokeService.html
-func (comp *Client) InvokeService(service string, api string, serviceData *power.HashMap, clientMsgID string) (*response.ResponseServiceMarketInvoceService, error) {
+func (comp *Client) InvokeService(options *request.RequestServiceMarket) (*response.ResponseServiceMarketInvoceService, error) {
 
 	result := &response.ResponseServiceMarketInvoceService{}
 
-	data := &object.HashMap{
-		"service":       service,
-		"api":           api,
-		"data":          serviceData,
-		"client_msg_id": clientMsgID,
-	}
-
-	_, err := comp.HttpPostJson("wxa/servicemarket", data, nil, nil, result)
+	_, err := comp.HttpPostJson("wxa/servicemarket", options, nil, nil, result)
 
 	return result, err
 }
