1014dc39a567f142f85d60ef3688aaaaf5cff249
Matrix-X <matrix-x@aritsan-cloud.com>
2022-05-20 15:31:54 +0800

    feature(officialAccount): session customer service Component

--- src/officialAccount/customerService/session/client.go
+++ src/officialAccount/customerService/session/client.go
@@ -1,7 +1,71 @@
 package session
 
-import "github.com/ArtisanCloud/PowerWeChat/src/kernel"
+import (
+	"github.com/ArtisanCloud/PowerLibs/object"
+	"github.com/ArtisanCloud/PowerWeChat/src/kernel"
+	"github.com/ArtisanCloud/PowerWeChat/src/kernel/power"
+	response2 "github.com/ArtisanCloud/PowerWeChat/src/kernel/response"
+	"github.com/ArtisanCloud/PowerWeChat/src/officialAccount/customerService/session/response"
+)
 
 type Client struct {
 	*kernel.BaseClient
+}
+
+// 获取客服会话列表
+// https://developers.weixin.qq.com/doc/offiaccount/Customer_Service/Session_control.html
+func (comp *Client) List() (*response.ResponseKFSessionList, error) {
+	result := &response.ResponseKFSessionList{}
+
+	_, err := comp.HttpGet("customservice/kfsession/getsessionlist", nil, nil, &result)
+
+	return result, err
+}
+
+// 获取未接入会话列表
+// https://developers.weixin.qq.com/doc/offiaccount/Customer_Service/Session_control.html
+func (comp *Client) Waiting() (*response.ResponseKFSessionWaitCaseList, error) {
+	result := &response.ResponseKFSessionWaitCaseList{}
+
+	_, err := comp.HttpGet("customservice/kfsession/getwaitcase", nil, nil, &result)
+
+	return result, err
+}
+
+// 创建会话
+// https://developers.weixin.qq.com/doc/offiaccount/Customer_Service/Session_control.html
+func (comp *Client) Create(account string, openID string) (*response2.ResponseOfficialAccount, error) {
+	result := &response2.ResponseOfficialAccount{}
+
+	params := &power.HashMap{
+		"kf_account": account,
+		"openid":     openID,
+	}
+	_, err := comp.HttpPostJson("customservice/kfsession/create", params, nil, nil, &result)
+
+	return result, err
+}
+
+// 关闭会话
+// https://developers.weixin.qq.com/doc/offiaccount/Customer_Service/Session_control.html
+func (comp *Client) Close(account string, openID string) (*response2.ResponseOfficialAccount, error) {
+	result := &response2.ResponseOfficialAccount{}
+
+	params := &power.HashMap{
+		"kf_account": account,
+		"openid":     openID,
+	}
+	_, err := comp.HttpPostJson("customservice/kfsession/close", params, nil, nil, &result)
+
+	return result, err
+}
+
+// 获取客户会话状态
+// https://developers.weixin.qq.com/doc/offiaccount/Customer_Service/Session_control.html
+func (comp *Client) Get(openID string) (*response.ResponseKFSessionGet, error) {
+	result := &response.ResponseKFSessionGet{}
+
+	_, err := comp.HttpGet("customservice/kfsession/getsession", &object.StringMap{"openid": openID}, nil, &result)
+
+	return result, err
 }
---
+++ src/officialAccount/customerService/session/response/responseKFSession.go
@@ -0,0 +1,38 @@
+package response
+
+import "github.com/ArtisanCloud/PowerWeChat/src/kernel/response"
+
+type Session struct {
+	CreateTime int    `json:"createtime"`
+	OpenID     string `json:"openid"`
+}
+
+type ResponseKFSessionList struct {
+	*response.ResponseOfficialAccount
+
+	Sessionlist []*Session `json:"sessionlist"`
+}
+
+// ---------------------------------------------------
+
+type WaitCase struct {
+	LatestTime int    `json:"latest_time"`
+	Openid     string `json:"openid"`
+}
+
+type ResponseKFSessionWaitCaseList struct {
+	*response.ResponseOfficialAccount
+
+	Count        int         `json:"count"`
+	WaitCaseList []*WaitCase `json:"waitcaselist"`
+}
+
+
+// ---------------------------------------------------
+
+type ResponseKFSessionGet struct {
+	*response.ResponseOfficialAccount
+
+	CreateTime int    `json:"createtime"`
+	KfAccount  string `json:"kf_account"`
+}
