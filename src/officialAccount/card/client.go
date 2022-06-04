17be0704b874fa9cdfe319e4fa1e33b95a600c3f
Matrix-X <matrix-x@aritsan-cloud.com>
2022-06-04 14:36:04 +0800

    feature(officialAccount): add card client

--- src/officialAccount/card/client.go
+++ src/officialAccount/card/client.go
@@ -4,4 +4,22 @@ import "github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
 
 type Client struct {
 	*kernel.BaseClient
+
+	URL               string
+	TicketCacheKey    string
+	TicketCachePrefix string
+}
+
+func NewClient(app kernel.ApplicationInterface) *Client {
+	client := &Client{
+		BaseClient: kernel.NewBaseClient(&app, nil),
+	}
+
+	client.TicketCachePrefix = "powerwechat.official_account.card.api_ticket."
+
+	return client
+}
+
+func (comp *Client) Colors() {
+
 }
