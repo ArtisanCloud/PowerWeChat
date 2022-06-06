adad32659dba1ffce46e40bc882078f261b24b37
Matrix-X <matrix-x@aritsan-cloud.com>
2022-06-06 14:51:11 +0800

    feature(officialAccount): create card and get card

--- src/officialAccount/card/client.go
+++ src/officialAccount/card/client.go
@@ -1,6 +1,11 @@
 package card
 
-import "github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
+import (
+	"github.com/ArtisanCloud/PowerLibs/v2/object"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount/card/request"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount/card/response"
+)
 
 type Client struct {
 	*kernel.BaseClient
@@ -21,5 +26,35 @@ func NewClient(app kernel.ApplicationInterface) *Client {
 }
 
 func (comp *Client) Colors() {
+
+}
+
+func (comp *Client) Categories() {
+
+}
+
+// 创建卡券
+// https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Create_a_Coupon_Voucher_or_Card.html
+func (comp *Client) Create(param *request.RequestCardCreate) (*response.ResponseCardCreate, error) {
+	result := &response.ResponseCardCreate{}
+
+	_, err := comp.HttpPostJson("card/create", param, nil, nil, result)
+
+	return result, err
+
+}
+
+// 查看卡券详情
+// https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#2
+func (comp *Client) Get(cardID string) (*response.ResponseCardGet, error) {
+	result := &response.ResponseCardGet{}
+
+	param := object.HashMap{
+		"card_id": cardID,
+	}
+
+	_, err := comp.HttpPostJson("card/get", param, nil, nil, result)
+
+	return result, err
 
 }
