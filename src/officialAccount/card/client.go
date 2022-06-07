3ebf1fd71440c81c2a3d8fe7a13bebb62589c70a
Matrix-X <matrix-x@aritsan-cloud.com>
2022-06-07 15:27:54 +0800

    feature(card): client add list , update, delete

--- src/officialAccount/card/client.go
+++ src/officialAccount/card/client.go
@@ -3,8 +3,10 @@ package card
 import (
 	"github.com/ArtisanCloud/PowerLibs/v2/object"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
+	response2 "github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/response"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount/card/request"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount/card/response"
+	"strings"
 )
 
 type Client struct {
@@ -54,6 +56,54 @@ func (comp *Client) Get(cardID string) (*response.ResponseCardGet, error) {
 	}
 
 	_, err := comp.HttpPostJson("card/get", param, nil, nil, result)
+
+	return result, err
+
+}
+
+// 批量查询卡券列表
+// https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#2
+func (comp *Client) List(offset int, count int, statusList string) (*response.ResponseCardList, error) {
+	result := &response.ResponseCardList{}
+
+	param := object.HashMap{
+		"offset":      offset,
+		"count":       count,
+		"status_list": statusList,
+	}
+
+	_, err := comp.HttpPostJson("card/batchget", param, nil, nil, result)
+
+	return result, err
+
+}
+
+// 更改卡券信息接口
+// https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#2
+func (comp *Client) Update(cardID string, cardType string, card request.CardInterface) (*response.ResponseCardUpdate, error) {
+	result := &response.ResponseCardUpdate{}
+
+	param := object.HashMap{
+		"card_id":                 cardID,
+		strings.ToLower(cardType): card,
+	}
+
+	_, err := comp.HttpPostJson("card/update", param, nil, nil, result)
+
+	return result, err
+
+}
+
+// 删除卡券接口
+// https://developers.weixin.qq.com/doc/offiaccount/Cards_and_Offer/Managing_Coupons_Vouchers_and_Cards.html#2
+func (comp *Client) Delete(cardID string) (*response2.ResponseOfficialAccount, error) {
+	result := &response2.ResponseOfficialAccount{}
+
+	param := object.HashMap{
+		"card_id": cardID,
+	}
+
+	_, err := comp.HttpPostJson("card/delete", param, nil, nil, result)
 
 	return result, err
 
