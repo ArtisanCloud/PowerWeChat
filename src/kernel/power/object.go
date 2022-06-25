8221f5e917558e8c33fef31f25b661d854eac996
Matrix-X <matrix-x@aritsan-cloud.com>
2022-06-25 16:28:30 +0800

    fix(payment): red pack add sendlistID

--- src/kernel/power/object.go
+++ src/kernel/power/object.go
@@ -1,1 +1,43 @@
 package power
+
+import "time"
+
+// Time 复制 time.Time 对象，并返回复制体的指针
+func Time(t time.Time) *time.Time {
+	return &t
+}
+
+// String 复制 string 对象，并返回复制体的指针
+func String(s string) *string {
+	return &s
+}
+
+// Bool 复制 bool 对象，并返回复制体的指针
+func Bool(b bool) *bool {
+	return &b
+}
+
+// Float64 复制 float64 对象，并返回复制体的指针
+func Float64(f float64) *float64 {
+	return &f
+}
+
+// Float32 复制 float32 对象，并返回复制体的指针
+func Float32(f float32) *float32 {
+	return &f
+}
+
+// Int64 复制 int64 对象，并返回复制体的指针
+func Int64(i int64) *int64 {
+	return &i
+}
+
+// Int32 复制 int64 对象，并返回复制体的指针
+func Int32(i int32) *int32 {
+	return &i
+}
+
+// Int 复制 int 对象，并返回复制体的指针
+func Int(i int) *int {
+	return &i
+}
