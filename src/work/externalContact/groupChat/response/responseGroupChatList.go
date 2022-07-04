b736936d4afaf05af08ecb295173d7ee4ebb8614
Matrix-X <matrix-x@aritsan-cloud.com>
2022-07-04 20:58:15 +0800

    refact(work): external conatct group detail with struct

--- src/work/externalContact/groupChat/response/responseGroupChatList.go
+++ src/work/externalContact/groupChat/response/responseGroupChatList.go
@@ -4,8 +4,13 @@ import (
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/response"
 )
 
+type CompactGroupChat struct {
+	ChatID string `json:"chat_id"`
+	Status int    `json:"status"`
+}
+
 type ResponseGroupChatList struct {
 	*response.ResponseWork
-	GroupChatList []*ResponseFailedChat `json:"group_chat_list"`
-	NextCursor    string                `json:"next_cursor"`
+	GroupChatList []*CompactGroupChat `json:"group_chat_list"`
+	NextCursor    string              `json:"next_cursor"`
 }
