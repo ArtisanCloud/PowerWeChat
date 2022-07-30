27633e5d49fa039f04693b049cd6f75f6a496756
Matrix-X <matrix-x@aritsan-cloud.com>
2022-07-30 23:44:33 +0800

    fix(wecom): group msg structure

--- src/work/externalContact/messageTemplate/response/responseGetGroupMsgSendResult.go
+++ src/work/externalContact/messageTemplate/response/responseGetGroupMsgSendResult.go
@@ -1,13 +1,21 @@
 package response
 
 import (
-	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/power"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/response"
 )
+
+type SendResult struct {
+	ExternalUserID string `json:"external_userid"`
+	ChatID         string `json:"chat_id"`
+	UserID         string `json:"userid"`
+	Status         int    `json:"status"`
+	SendTime       int    `json:"send_time"`
+}
 
 type ResponseGetGroupMsgSendResult struct {
 	*response.ResponseWork
 
-	NextCursor string           `json:"next_cursor"`
-	SendList   []*power.HashMap `json:"send_list"`
+	NextCursor string `json:"next_cursor"`
+
+	SendList []*SendResult `json:"send_list"`
 }
--- src/work/externalContact/messageTemplate/response/responseGetGroupMsgTask.go
+++ src/work/externalContact/messageTemplate/response/responseGetGroupMsgTask.go
@@ -1,13 +1,19 @@
 package response
 
 import (
-	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/power"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/response"
 )
+
+type Task struct {
+	UserID   string `json:"userid"`
+	Status   int    `json:"status"`
+	SendTime int    `json:"send_time"`
+}
 
 type ResponseGetGroupMsgTask struct {
 	*response.ResponseWork
 
-	NextCursor string           `json:"next_cursor"`
-	TaskList   []*power.HashMap `json:"task_list"`
+	NextCursor string `json:"next_cursor"`
+
+	TaskList []*Task `json:"task_list"`
 }
