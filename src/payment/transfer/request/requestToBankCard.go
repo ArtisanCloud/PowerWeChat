b3fb852c8172c1715d2a22491780a9d7f5ba3135
Matrix-X <matrix-x@aritsan-cloud.com>
2022-08-05 13:02:42 +0800

    fix(payment): fix function ToBankCard, change type of  PartnerTradeNO and MchID to string in struct RequestToBankCard

--- src/payment/transfer/request/requestToBankCard.go
+++ src/payment/transfer/request/requestToBankCard.go
@@ -1,13 +1,12 @@
 package request
 
 type RequestToBankCard struct {
-
-	Amount         int64   `xml:"amount" json:"amount"`
-	BankCode       string   `xml:"bank_code" json:"bank_code"`
-	Desc           string   `xml:"desc" json:"desc"`
-	EncBankNO      string   `xml:"enc_bank_no" json:"enc_bank_no"`
-	EncTrueName    string   `xml:"enc_true_name" json:"enc_true_name"`
-	MchID          int64   `xml:"mch_id" json:"mch_id"`
-	NonceStr       string   `xml:"nonce_str" json:"nonce_str"`
-	PartnerTradeNO int64    `xml:"partner_trade_no" json:"partner_trade_no"`
+	Amount         int64  `xml:"amount" json:"amount"`
+	BankCode       string `xml:"bank_code" json:"bank_code"`
+	Desc           string `xml:"desc" json:"desc"`
+	EncBankNO      string `xml:"enc_bank_no" json:"enc_bank_no"`
+	EncTrueName    string `xml:"enc_true_name" json:"enc_true_name"`
+	MchID          string `xml:"mch_id" json:"mch_id"`
+	NonceStr       string `xml:"nonce_str" json:"nonce_str"`
+	PartnerTradeNO string `xml:"partner_trade_no" json:"partner_trade_no"`
 }
