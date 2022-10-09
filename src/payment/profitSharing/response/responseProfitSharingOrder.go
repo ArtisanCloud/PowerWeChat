4c0e5e94c0b0f696df6586785598162cac651e07
Matrix-X <matrix-x@aritsan-cloud.com>
2022-10-09 22:19:04 +0800

    fix(payment): request.Receiver for request and response type of Share

--- src/payment/profitSharing/response/responseProfitSharingOrder.go
+++ src/payment/profitSharing/response/responseProfitSharingOrder.go
@@ -1,16 +1,16 @@
 package response
 
 import (
-	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/power"
 	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel/response"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/payment/profitSharing/request"
 )
 
 type ResponseProfitSharingOrder struct {
 	response.ResponsePayment
 
-	TransactionID string           `json:"transaction_id"`
-	OutOrderNO    string           `json:"out_order_no"`
-	OrderID       string           `json:"order_id"`
-	State         string           `json:"state"`
-	Receivers     []*power.HashMap `json:"receivers"`
+	TransactionID string              `json:"transaction_id"`
+	OutOrderNO    string              `json:"out_order_no"`
+	OrderID       string              `json:"order_id"`
+	State         string              `json:"state"`
+	Receivers     []*request.Receiver `json:"receivers"`
 }
