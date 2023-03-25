6607564fe05bb3d3998c687c0629c9d5e65dad0f
Matrix-X <matrix-x@aritsan-cloud.com>
2023-03-25 22:39:46 +0800

    feat(payment): add get public key for api

--- src/payment/transfer/client.go
+++ src/payment/transfer/client.go
@@ -100,8 +100,10 @@ func (comp *Client) ToBankCard(ctx context.Context, data *request.RequestToBankC
 	//}
 	//rsaSigner.RSAEncryptor.PublicKeyPath = config.GetString("rsa_public_key_path", "")
 	//_, err = rsaSigner.RSAEncryptor.LoadPublicKeyByPath()
-	if comp.RsaOAEP == nil {
-		return nil, errors.New("支付模块的RsaOAEP配置有误，要加密敏感信息，请确保正确配置EncryptOAEP的公钥路径")
+	if comp.RsaOAEP == nil ||
+		comp.RsaOAEP.RSAEncryptor.PrivateKey == nil ||
+		comp.RsaOAEP.RSAEncryptor.PublicKey == nil {
+		return nil, errors.New("支付模块的RsaOAEP配置有误，要加密敏感信息，请确保正确配置EncryptOAEP的证书路径")
 	}
 
 	buffer, err := comp.RsaOAEP.RSAEncryptor.Encrypt([]byte(data.EncBankNO))
