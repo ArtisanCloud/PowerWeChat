70294eccc4506020cbe4c27c4c93a003fcb7b033
Matrix-X <matrix-x@aritsan-cloud.com>
2023-04-18 21:24:31 +0800

    fix(payment): profit sharing RequestShare UnfreezeUnSplit without omitempty

--- src/payment/profitSharing/request/share.go
+++ src/payment/profitSharing/request/share.go
@@ -10,7 +10,7 @@ type RequestShare struct {
 	TransactionID   string      `json:"transaction_id,omitempty"` // OutTradeNo 和 TransactionID 二选一
 	OutOrderNO      string      `json:"out_order_no,omitempty"`
 	Receivers       []*Receiver `json:"receivers,omitempty"`
-	UnfreezeUnSplit bool        `json:"unfreeze_unsplit,omitempty"`
+	UnfreezeUnSplit bool        `json:"unfreeze_unsplit"`
 }
 
 type Receiver struct {
