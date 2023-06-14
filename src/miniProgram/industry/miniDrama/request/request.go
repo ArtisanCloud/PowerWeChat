8f712700af66b67dbe4d738088573c0ea953e110
alvin <34093484@qq.com>
2023-06-14 11:35:34 +0800

    短剧媒资管理相关api

--- src/miniProgram/industry/miniDrama/request/request.go
+++ src/miniProgram/industry/miniDrama/request/request.go
@@ -1,1 +1,55 @@
 package request
+
+// 该接口用于将一个网络上的视频拉取上传到平台。
+// 不填写 cover_url 字段时默认截取视频首帧作为封面。
+// Content-Type 需要指定为 application/json
+// 该接口为异步接口，上传完成会推送上传完成事件到开发者服务器，开发者也可以调用"查询任务"接口来轮询上传结果。
+
+type VideoMediaUploadByURLRequest struct {
+	MediaUrl      string `json:"media_url"`                // 视频 URL，示例值："https://developers.weixin.qq.com/test.mp4"。
+	CoverUrl      string `json:"cover_url"`                // 封面图片 URL，示例值："https://developers.weixin.qq.com/test.jpg"
+	MediaName     string `json:"media_name"`               // 文件名，需按照“剧目名 - 对应剧集数”格式命名文件，示例值："我的中国梦 - 第1集"。
+	SourceContext string `json:"source_context,omitempty"` // 来源上下文，会在上传完成事件中透传给开发者。
+}
+
+// 获取媒资列表
+// media_name 参数支持模糊匹配，当需要模糊匹配时可以在前面或后面加上 %，否则为精确匹配。例如 "test%" 可以匹配到 "test123", "testxxx", "test"。
+
+type MediaListRequest struct {
+	DramaId   int64  `json:"drama_id,omitempty"`   // 根据剧目id获取剧集信息。
+	MediaName string `json:"media_name,omitempty"` // 媒资文件名，模糊匹配。
+	StartTime int64  `json:"start_time,omitempty"` // 媒资上传时间>=start_time。
+	EndTime   int64  `json:"end_time,omitempty"`   // 媒资上传时间<end_time。
+	Limit     int64  `json:"limit,omitempty"`      // 分页拉取的最大返回结果数。默认值：100；最大值：100。
+	Offset    int64  `json:"offset,omitempty"`     // 分页拉取的起始偏移量。默认值：0。
+}
+
+// 获取媒资播放连接
+type GetMediaLinkRequest struct {
+	MediaId string `json:"media_id"`         // 媒资id。
+	T       int64  `json:"t"`                // 播放地址的过期时间戳。有效的时间最长不能超过2小时后。
+	Us      string `json:"us,omitempty"`     // 链接标识。平台默认会生成一个仅包含小写字母和数字的字符串用于增强链接的唯一性(如us=647488c4792c15185b8fd2a6)。如开发者需要增加自己的标识，比如区分播放的渠道，可使用该参数，该参数最终的值是"开发者标识-平台标识"（如开发者传入abcd，则最终的临时链接中us=abcd-647488c4792c15185b8fd2a6）
+	Expr    int64  `json:"expr,omitempty"`   // 试看时长，单位：秒，最大值不能超过视频长度
+	Rlimit  int64  `json:"rlimit,omitempty"` // 最多允许多少个不同 IP 的终端播放，以十进制表示，最大值为9，不填表示不做限制。当限制 URL 只能被1个人播放时，建议 rlimit 不要严格限制成1（例如可设置为3），因为移动端断网后重连 IP 可能改变。
+	Hhref   string `json:"whref,omitempty"`  // 允许访问的域名列表，支持1条 - 10条，用半角逗号分隔。域名前不要带协议名（http://和https://），域名为前缀匹配（如填写 abc.com，则 abc.com/123 和 abc.com.cn也会匹配），且支持通配符（如 *.abc.com）。
+	Bkref   string `json:"bkref,omitempty"`  // 禁止访问的域名列表，支持1条 - 10条，用半角逗号分隔。域名前不要带协议名（http://和https://），域名为前缀匹配（如填写 abc.com，则 abc.com/123 和 abc.com.cn也会匹配），且支持通配符（如 *.abc.com）。
+}
+
+// 剧目提审
+type SubmitAuditRequest struct {
+	MediaId                  string  `json:"media_id,omitempty"`                    // 剧目id，首次提审不需要填该参数，重新提审时必填
+	Name                     string  `json:"name,omitempty"`                        // 剧名，首次提审时必填，重新提审时根据是否需要修改选填。第一次必须填写！
+	MediaCount               int64   `json:"media_count,omitempty"`                 // 剧集数目。首次提审时必填， 重新提审时可不填，如要填写也要和第一次提审时一样。
+	MediaIdList              []int64 `json:"media_id_list,omitempty"`               // 剧集媒资media_id列表。首次提审时必填，而且元素个数必须与media_count一致。重新提审时，如果剧集有内容有变化，则需要填写，而且元素个数也必须与 media_count一致。
+	Producer                 string  `json:"producer,omitempty"`                    // 制作方 。首次提审时必填，重新提审时根据是否需要修改选填。
+	CoverMaterialId          string  `json:"cover_material_id,omitempty"`           // 封面图片临时media_id。首次提审时必填，重新提审时根据是否需要修改选填。
+	AuthorizedMaterialId     string  `json:"authorized_material_id,omitempty"`      // 《网络剧片发行许可证编号》。剧目播放授权材料material_id。如果小程序主体名称和制作方完全一致，则不需要填，否则必填
+	RegistrationNumber       string  `json:"registration_number,omitempty"`         // 《剧目备案号》。首次提审时《剧目备案号》与《网络剧片发行许可证编号》二选一。重新提审时根据是否需要修改选填
+	PublishLicense           string  `json:"publish_license,omitempty"`             // 网络剧片发行许可证《编号》。首次提审时《剧目备案号》与《网络剧片发行许可证编号》二选一。重新提审时根据是否需要修改选填
+	PublishLicenseMaterialId string  `json:"publish_license_material_id,omitempty"` // 网络剧片发行许可证《图片》，首次提审时如果网《络剧片发行许可证编号》非空，则该改字段也非空。重新提审时根据是否变化选填
+}
+
+type GetDramaListRequest struct {
+	Limit  int64 `json:"limit,omitempty"`  // 分页拉取的最大返回结果数。默认值：100；最大值：100。
+	Offset int64 `json:"offset,omitempty"` // 分页拉取的起始偏移量。默认值：0。
+}
--- src/miniProgram/industry/miniDrama/vod/client.go
+++ src/miniProgram/industry/miniDrama/vod/client.go
@@ -1,7 +1,221 @@
 package vod
 
-import "github.com/ArtisanCloud/PowerWeChat/v3/src/kernel"
+import (
+	"context"
+	"errors"
+	"github.com/ArtisanCloud/PowerWeChat/v3/src/kernel"
+	"github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/power"
+	"github.com/ArtisanCloud/PowerWeChat/v3/src/miniProgram/industry/miniDrama/request"
+	"github.com/ArtisanCloud/PowerWeChat/v3/src/miniProgram/industry/miniDrama/response"
+)
 
 type Client struct {
 	BaseClient *kernel.BaseClient
+}
+
+// 拉取上传 该接口使用url传递到微信服务器
+//https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_1-2-%E6%8B%89%E5%8F%96%E4%B8%8A%E4%BC%A0
+
+func (comp *Client) VideoMediaUploadByURL(ctx context.Context, in *request.VideoMediaUploadByURLRequest) (result *response.VideoMediaUploadByURLResponse, err error) {
+
+	params, err := power.StructToHashMap(in)
+
+	if err != nil {
+
+		return nil, err
+	}
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/pullupload", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+}
+
+// 查询任务 通过上传接口获得的taskid 进行查询
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_1-2-%E6%8B%89%E5%8F%96%E4%B8%8A%E4%BC%A0
+
+func (comp *Client) SearchMediaByTaskId(ctx context.Context, taskId string) (result *response.VideoMediaSearchTaskResponse, err error) {
+
+	params := &power.HashMap{
+		"task_id": taskId,
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/gettask", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+
+}
+
+// 获取媒资列表 该接口用于获取已上传到平台的媒资列表。
+// 本接口返回的视频或图片链接均为临时链接，不应将其保存下来。
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_2-1-%E8%8E%B7%E5%8F%96%E5%AA%92%E8%B5%84%E5%88%97%E8%A1%A8
+
+func (comp *Client) GetMediaList(ctx context.Context, in *request.MediaListRequest) (result *response.MediaListResponse, err error) {
+
+	if in.Limit > 100 {
+		in.Limit = 100
+	}
+
+	if in.Offset <= 0 {
+		in.Offset = 0
+	}
+
+	params, err := power.StructToHashMap(in)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/listmedia", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+
+}
+
+// 获取媒资详细信息
+// 该接口用于获取已上传到平台的指定媒资信息，用于开发者后台管理使用。用于给用户客户端播放的链接应该使用getmedialink接口获取
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_2-2-%E8%8E%B7%E5%8F%96%E5%AA%92%E8%B5%84%E8%AF%A6%E7%BB%86%E4%BF%A1%E6%81%AF
+
+func (comp *Client) GetMediaInfo(ctx context.Context, mediaId string) (result *response.MediaInfoResponse, err error) {
+
+	params := &power.HashMap{
+		"media_id": mediaId,
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/getmedia", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+
+}
+
+// 获取媒资播放链接
+// 需要提审后才能调用该接口，不然会返回未审核通过
+// 该接口用于获取视频临时播放链接，用于给用户的播放使用。只有审核通过的视频才能通过该接口获取播放链接。
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_2-3-%E8%8E%B7%E5%8F%96%E5%AA%92%E8%B5%84%E6%92%AD%E6%94%BE%E9%93%BE%E6%8E%A5
+
+func (comp *Client) GetMediaLink(ctx context.Context, in *request.GetMediaLinkRequest) (result *response.MediaLinkResponse, err error) {
+
+	params, err := power.StructToHashMap(in)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/getmedialink", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+
+}
+
+// 删除媒资
+// 该接口用于删除指定媒资。
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_2-4-%E5%88%A0%E9%99%A4%E5%AA%92%E8%B5%84
+
+func (comp *Client) DeleteMedia(ctx context.Context, mediaId int64) (result *response.BaseResponse, err error) {
+
+	params := &power.HashMap{
+		"media_id": mediaId,
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/deletemedia", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+}
+
+// 剧目提审
+// 该接口用于提交剧目审核。
+// 剧目信息与审核材料在首次提审时为必填，重新提审时根据是否需要修改选填，
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_3-1-%E5%89%A7%E7%9B%AE%E6%8F%90%E5%AE%A1
+
+func (comp *Client) SubmitAudit(ctx context.Context, in *request.SubmitAuditRequest) (result *response.SubmitAuditResponse, err error) {
+
+	params, err := power.StructToHashMap(in)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/submitaudit", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+}
+
+// 获取剧目列表
+// 该接口用于获取已提交的剧目列表。
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_3-2-%E8%8E%B7%E5%8F%96%E5%89%A7%E7%9B%AE%E5%88%97%E8%A1%A8
+
+func (comp *Client) GetDramaList(ctx context.Context, in *request.GetDramaListRequest) (result *response.GetDramaListResponse, err error) {
+
+	params, err := power.StructToHashMap(in)
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/getdramalist", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
+}
+
+// 获取剧目信息
+// 该接口用于查询已提交的剧目。
+// 本接口返回的图片链接均为临时链接，不应将其保存下来。
+// https://developers.weixin.qq.com/miniprogram/dev/platform-capabilities/industry/mini-drama/mini_drama.html#_3-3-%E8%8E%B7%E5%8F%96%E5%89%A7%E7%9B%AE%E4%BF%A1%E6%81%AF
+
+func (comp *Client) GetDramaInfo(ctx context.Context, dramaId string) (result *response.GetDramaInfoResponse, err error) {
+
+	params := &power.HashMap{
+		"drama_id": dramaId,
+	}
+
+	_, err = comp.BaseClient.HttpPostJson(ctx, "wxa/sec/vod/getdramainfo", params, nil, nil, &result)
+
+	if result.Errcode != 0 {
+
+		return nil, errors.New(result.Errmsg)
+	}
+
+	return
 }
