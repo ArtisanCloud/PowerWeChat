edbd0a30b17d215645aeb65ed7259bab8a90581f
Matrix-X <matrix-x@aritsan-cloud.com>
2024-02-23 15:16:14 +0800

    update(official): event with ticket field

--- src/officialAccount/server/handlers/models/event.go
+++ src/officialAccount/server/handlers/models/event.go
@@ -33,12 +33,14 @@ type EventSubscribe struct {
 	models.CallbackMessageHeader
 	EventKey string `xml:"EventKey"`
 	AgentID  string `xml:"AgentID"`
+	Ticket   string `xml:"Ticket"`
 }
 
 type EventUnSubscribe struct {
 	contract.EventInterface
 	models.CallbackMessageHeader
 	AgentID string `xml:"AgentID"`
+	Ticket  string `xml:"Ticket"`
 }
 
 type EventEnterAgent struct {
