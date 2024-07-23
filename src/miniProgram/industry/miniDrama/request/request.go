db195db7a6a163faaa2f3fe915b81938d8e80f65
LPF <lpf929@126.com>
2024-07-23 14:54:42 +0800

    miniDrama | Complete functional access

--- src/miniProgram/industry/miniDrama/request/request.go
+++ src/miniProgram/industry/miniDrama/request/request.go
@@ -12,6 +12,41 @@ type VideoMediaUploadByURLRequest struct {
 	SourceContext string `json:"source_context,omitempty"` // 来源上下文，会在上传完成事件中透传给开发者。
 }
 
+// 该接口用于将服务端本地文件上传到平台
+type VideoMediaUploadByFileRequest struct {
+	MediaName     string `json:"media_name"`               // 文件名，需按照“剧目名 - 对应剧集数”格式命名文件，示例值："我的中国梦 - 第1集"。 经测试 可以不用严格按照此格式
+	MediaType     string `json:"media_type"`               // 视频格式，支持：MP4，TS，MOV，MXF，MPG，FLV，WMV，AVI，M4V，F4V，MPEG，3GP，ASF，MKV，示例值："MP4"。
+	MediaData     string `json:"media_data"`               // 视频服务端路径
+	CoverType     string `json:"cover_type,omitempty"`     // 视频封面图格式，支持：JPG、JPEG、PNG、BMP、TIFF、AI、CDR、EPS、TIF，示例值："JPG"。
+	CoverData     string `json:"cover_data,omitempty"`     // 视频封面图文件内容，二进制。
+	SourceContext string `json:"source_context,omitempty"` // 来源上下文，会在上传完成事件中透传给开发者。
+}
+
+type VideoApplyChunkUploadByIdRequest struct {
+	MediaName     string `json:"media_name"`               // 文件名，需按照“剧目名 - 对应剧集数”格式命名文件，示例值："我的中国梦 - 第1集"。 经测试 可以不用严格按照此格式
+	MediaType     string `json:"media_type"`               // 视频格式，支持：MP4，TS，MOV，MXF，MPG，FLV，WMV，AVI，M4V，F4V，MPEG，3GP，ASF，MKV，示例值："MP4"。
+	CoverType     string `json:"cover_type,omitempty"`     // 视频封面图格式，支持：JPG、JPEG、PNG、BMP、TIFF、AI、CDR、EPS、TIF，示例值："JPG"。
+	SourceContext string `json:"source_context,omitempty"` // 来源上下文，会在上传完成事件中透传给开发者。
+}
+
+type VideoApplyChunkUploadRequest struct {
+	UploadId     string `json:"upload_id"`     // 分片上传ID 必填
+	ChunkId      int64  `json:"chunk_id"`      // 分片唯一ID
+	ChunkPath    string `json:"chunk_path"`    // 文件路径
+	ResourceType int64  `json:"resource_type"` // 指定该分片属于视频还是图片的枚举值：1. 视频，2. 图片。
+}
+
+type VideoChunkUploadCompleteRequest struct {
+	UploadId       string      `json:"upload_id"`                  // 分片上传ID 必填
+	MediaPartInfos []*PartInfo `json:"media_part_infos"`           // 本次分片上传中媒体文件每个分片的信息。
+	CoverPartInfos []*PartInfo `json:"cover_part_infos,omitempty"` // 本次分片上传中封面图片文件每个分片的信息。
+}
+
+type PartInfo struct {
+	PartNumber int64  `json:"part_number"` // 分片编号。
+	Etag       string `json:"etag"`        // 使用上传分片接口上传成功后返回的 etag 的值
+}
+
 // 获取媒资列表
 // media_name 参数支持模糊匹配，当需要模糊匹配时可以在前面或后面加上 %，否则为精确匹配。例如 "test%" 可以匹配到 "test123", "testxxx", "test"。
 
@@ -37,21 +72,47 @@ type GetMediaLinkRequest struct {
 
 // 剧目提审
 type SubmitAuditRequest struct {
-	DramaId                  int64   `json:"drama_id,omitempty"`                    // 剧目id，首次提审不需要填该参数，重新提审时必填
-	Name                     string  `json:"name,omitempty"`                        // 剧名，首次提审时必填，重新提审时根据是否需要修改选填。第一次必须填写！
-	MediaCount               int64   `json:"media_count,omitempty"`                 // 剧集数目。首次提审时必填， 重新提审时可不填，如要填写也要和第一次提审时一样。
-	MediaIdList              []int64 `json:"media_id_list,omitempty"`               // 剧集媒资media_id列表。首次提审时必填，而且元素个数必须与media_count一致。重新提审时，如果剧集有内容有变化，则需要填写，而且元素个数也必须与 media_count一致。
-	Producer                 string  `json:"producer,omitempty"`                    // 制作方 。首次提审时必填，重新提审时根据是否需要修改选填。
-	CoverMaterialId          string  `json:"cover_material_id,omitempty"`           // 封面图片临时media_id。首次提审时必填，重新提审时根据是否需要修改选填。
-	AuthorizedMaterialId     string  `json:"authorized_material_id,omitempty"`      // 《网络剧片发行许可证编号》。剧目播放授权材料material_id。如果小程序主体名称和制作方完全一致，则不需要填，否则必填
-	RegistrationNumber       string  `json:"registration_number,omitempty"`         // 《剧目备案号》。首次提审时《剧目备案号》与《网络剧片发行许可证编号》二选一。重新提审时根据是否需要修改选填
-	PublishLicense           string  `json:"publish_license,omitempty"`             // 网络剧片发行许可证《编号》。首次提审时《剧目备案号》与《网络剧片发行许可证编号》二选一。重新提审时根据是否需要修改选填
-	PublishLicenseMaterialId string  `json:"publish_license_material_id,omitempty"` // 网络剧片发行许可证《图片》，首次提审时如果网《络剧片发行许可证编号》非空，则该改字段也非空。重新提审时根据是否变化选填
+	DramaId                            int64          `json:"drama_id"`                                        // 剧目id，首次提审不需要填该参数，重新提审时必填
+	Name                               string         `json:"name"`                                            // 剧名，首次提审时必填，重新提审时根据是否需要修改选填。第一次必须填写！
+	MediaCount                         int64          `json:"media_count"`                                     // 剧集数目。首次提审时必填， 重新提审时可不填，如要填写也要和第一次提审时一样。
+	MediaIdList                        []int64        `json:"media_id_list"`                                   // 剧集媒资media_id列表。首次提审时必填，而且元素个数必须与media_count一致。重新提审时，如果剧集有内容有变化，则需要填写，而且元素个数也必须与 media_count一致。
+	Description                        string         `json:"description"`                                     // 剧目简介，可填写200个字符。
+	Recommendations                    string         `json:"recommendations"`                                 // 剧目推荐语，可填写15个字符。
+	CoverMaterialId                    string         `json:"cover_material_id"`                               // 剧目海报临时media_id。首次提审时必填，重新提审时根据是否需要修改选填。
+	PromotionPosterMaterialId          string         `json:"promotion_poster_material_id,omitempty"`          // 推广海报临时media_id。
+	Producer                           string         `json:"producer"`                                        // 制作方 。首次提审时必填，重新提审时根据是否需要修改选填。
+	AuthorizedMaterialId               string         `json:"authorized_material_id"`                          // 权利声明/播放授权材料material_id。 需上传《权利声明及不侵权承诺函》
+	QualificationType                  int64          `json:"qualification_type"`                              // 剧目资质：1-取得《网络剧片发行许可证》或重点节目备案号；2-未取得《网络剧片发行许可证》或重点节目备案，且制作成本小于30万元
+	RegistrationNumber                 string         `json:"registration_number,omitempty"`                   // 剧目备案号，当qualification_type=1时必填。根据提供的剧目资质证明文件填写对应的网络剧片发行许可证编号或剧目备案号。如：(沪)网剧审字(2023)第001号 或 V123456788888888
+	QualificationCertificateMaterialId string         `json:"qualification_certificate_material_id,omitempty"` // 剧目资质证明文件，当qualification_type=1时必填。请提供网络剧片发行许可证或广电备案系统截图
+	CostCommitmentLetterMaterialId     string         `json:"cost_commitment_letter_material_id,omitempty"`    // 《成本配置比例情况报告》material_id，当qualification_type=2时必填。
+	CostOfProduction                   int64          `json:"cost_of_production,omitempty"`                    // 剧目制作成本（单位：万元），当qualification_type=2时必填。请填写“1-29” 的整数（如非整数将截断取整），数值需与《成本配置比例情况报告》中对应剧目制作成本一致。
+	Expedited                          int64          `json:"expedited,omitempty"`                             // 填1表示审核加急，0或不填为不加急。每天有5次加急机会。该字段在首次提审时才有效，重新提审时会沿用首次提审时的属性，重新提审不会扣次数。最终是否为加急单，可以根据DramaInfo.expedited属性判断
+	ActorList                          *ActorList     `json:"actor_list"`                                      // 演员信息，需填写2-5位演员。
+	ReplaceMediaList                   []*ReplaceInfo `json:"replace_media_list,omitempty"`                    // 用于重新提审时替换审核不通过的剧集。
 }
 
-type GetDramaListRequest struct {
+type ReplaceInfo struct {
+	Old int64 `json:"old"`
+	New int64 `json:"new"`
+}
+
+type ListRequest struct {
 	Limit  int64 `json:"limit,omitempty"`  // 分页拉取的最大返回结果数。默认值：100；最大值：100。
 	Offset int64 `json:"offset,omitempty"` // 分页拉取的起始偏移量。默认值：0。
+}
+
+// 剧目审核
+type SubmitReplaceDramaAuditRequest struct {
+	DramaId          int64          `json:"drama_id"`
+	ReplaceMediaList []*ReplaceInfo `json:"replace_media_list"`
+}
+
+// 替换审核过的剧集
+type ReplaceAuditDramaRequest struct {
+	DramaId    int64 `json:"drama_id"`
+	OldMediaId int64 `json:"old_media_id"`
+	NewMediaId int64 `json:"new_media_id"`
 }
 
 // 剧目基本信息修改
@@ -63,9 +124,10 @@ type UpdateDramaInfoRequest struct {
 	PromotionPosterMaterialId          string     `json:"promotion_poster_material_id,omitempty"`
 	AlternateName                      string     `json:"alternate_name,omitempty"`
 	ActorList                          *ActorList `json:"actor_list,omitempty"`
-	QualificationType                  int        `json:"qualification_type,omitempty"`
+	QualificationType                  int64      `json:"qualification_type,omitempty"`
 	QualificationCertificateMaterialId string     `json:"qualification_certificate_material_id,omitempty"`
 	RegistrationNumber                 string     `json:"registration_number,omitempty"`
+	CostOfProduction                   int64      `json:"cost_of_production,omitempty"`
 	CostCommitmentLetterMaterialId     string     `json:"cost_commitment_letter_material_id,omitempty"`
 }
 
@@ -74,8 +136,46 @@ type ActorList struct {
 }
 
 type Actor struct {
-	Name            string `json:"name"`
-	PhotoMaterialId string `json:"photo_material_id"`
-	Role            string `json:"role"`
-	Profile         string `json:"profile"`
+	Name            string `json:"name"`              // 演员姓名，格式要求：可填写30个字符，支持输入中文、英文和·
+	PhotoMaterialId string `json:"photo_material_id"` // 演员照片临时media_id
+	Role            string `json:"role"`              // 饰演角色，格式要求：可填写30个字符，支持输入中文、英文及，。；”“？、—《》！（）-·
+	Profile         string `json:"profile"`           // 演员简介，填写内容可参考：生日、星座、籍贯、身高、毕业院校、历史/代表作品、获奖信息、成就等(以上要素需至少满足3项或涵盖任一要素且不少于20字）。格式要求：可填写100个字符，支持输入中文、英文、阿拉伯数字及，。：；“”？、—《》！（）-·
+}
+
+// 查询剧目审核信息
+type SearchDramaAuditInfoRequest struct {
+	DramaId   int64 `json:"drama_id"`   // 剧目id
+	AuditType int64 `json:"audit_type"` // 审核类型。0为首次提审；1为再次提审；2为替换剧集提审；3为修改剧目基本信息审核。
+}
+
+// 查询cdn
+type SearchCdnInfoRequest struct {
+	StartTime    int64 `json:"start_time"`
+	EndTime      int64 `json:"end_time"`
+	DataInterval int64 `json:"data_interval,omitempty"`
+}
+
+// 剧目授权
+type DramaAuthorizedRequest struct {
+	AuthorizedAppid string  `json:"authorized_appid"`
+	DramaId         []int64 `json:"drama_id"`
+	AuthzExpireTime int64   `json:"authz_expire_time,omitempty"`
+}
+
+// 查询剧目授权
+type SearchDramaAuthorizedRequest struct {
+	DramaId int64 `json:"drama_id"`
+	ListRequest
+}
+
+// 查询剧目被授权
+type SearchDoDramaAuthorizedRequest struct {
+	AuthorizerAppid string `json:"authorizer_appid"`
+	ListRequest
+}
+
+// 查询账号授权
+type AccountAuthorizedRequest struct {
+	AuthorizedAppid string `json:"authorized_appid"`
+	AuthzExpireTime int64  `json:"authz_expire_time,omitempty"`
 }
--- src/miniProgram/industry/miniDrama/response/response.go
+++ src/miniProgram/industry/miniDrama/response/response.go
@@ -6,14 +6,12 @@ type BaseResponse struct {
 }
 
 // 拉取上传Response
-
 type VideoMediaUploadByURLResponse struct {
 	BaseResponse
 	TaskId int64 `json:"task_id"` // 任务id 用于查询
 }
 
 // 查询任务 使用上面返回的task id 进行查询
-
 type VideoMediaSearchTaskResponse struct {
 	BaseResponse
 	TaskInfo struct {
@@ -28,7 +26,32 @@ type VideoMediaSearchTaskResponse struct {
 	} `json:"task_info"`
 }
 
-type AuditDetail struct { // 审核信息。 文档写了 但是接口没返回 自主加了 omitempty
+// 拉取上传Response
+type VideoMediaUploadByFileResponse struct {
+	BaseResponse
+	MediaId int64 `json:"media_id"` // 任务id 用于查询
+}
+
+// 分片上传Id申请
+type VideoApplyChunkUploadByIdResponse struct {
+	BaseResponse
+	UploadId string `json:"upload_id"`
+}
+
+// 分片上传结果
+type VideoApplyChunkUploadResponse struct {
+	BaseResponse
+	Etag string `json:"etag"`
+}
+
+// 分片上传最终结果
+type VideoChunkUploadCompleteResponse struct {
+	BaseResponse
+	MediaId string `json:"media_id"` // 媒资ID
+}
+
+// 审核信息。 文档写了 但是接口没返回 自主加了 omitempty
+type AuditDetail struct {
 	Status                 int64    `json:"status,omitempty"`                    // 0为无效值；1为审核中；2为审核驳回；3为审核通过；
 	CreateTime             int64    `json:"create_time,omitempty"`               // 提审时间戳。
 	AuditTime              int64    `json:"audit_time,omitempty"`                // 审核时间戳。
@@ -36,7 +59,21 @@ type AuditDetail struct { // 审核信息。 文档写了 但是接口没返回 
 	EvidenceMaterialIdList []string `json:"evidence_material_id_list,omitempty"` // 审核证据截图id列表，截图id可以用作get_material接口的参数来获得截图内容。
 }
 
-type MediaInfo struct { // 媒资信息列表。
+// 演员列表
+type ActorList struct {
+	Actor []*Actor `json:"actor,omitempty"`
+}
+
+// 演员详细信息
+type Actor struct {
+	Name            string `json:"name"`              // 演员姓名，格式要求：可填写30个字符，支持输入中文、英文和·
+	PhotoMaterialId string `json:"photo_material_id"` // 演员照片临时media_id
+	Role            string `json:"role"`              // 饰演角色，格式要求：可填写30个字符，支持输入中文、英文及，。；”“？、—《》！（）-·
+	Profile         string `json:"profile"`           // 演员简介，填写内容可参考：生日、星座、籍贯、身高、毕业院校、历史/代表作品、获奖信息、成就等(以上要素需至少满足3项或涵盖任一要素且不少于20字）。格式要求：可填写100个字符，支持输入中文、英文、阿拉伯数字及，。：；“”？、—《》！（）-·
+}
+
+// 媒资信息列表。
+type MediaInfo struct {
 	MediaId     int64        `json:"media_id"`     // 媒资文件id。
 	CreateTime  int64        `json:"create_time"`  // 上传时间，时间戳。
 	ExpireTime  int64        `json:"expire_time"`  // 过期时间，时间戳。
@@ -58,8 +95,6 @@ type MediaListResponse struct {
 }
 
 // 查询媒资信息
-// 字段参考 MediaListResponse
-
 type MediaInfoResponse struct {
 	BaseResponse
 	MediaInfo MediaInfo `json:"media_info"`
@@ -76,36 +111,40 @@ type MediaLinkInfo struct {
 	OriginalUrl string `json:"original_url"`
 }
 
+// 媒资播放链接
 type MediaLinkResponse struct {
 	BaseResponse
 	MediaInfo MediaLinkInfo `json:"media_info"`
 }
 
 // 剧目提审
-
 type SubmitAuditResponse struct {
 	BaseResponse
 	DramaId int64 `json:"drama_id"` // // 剧目id。
 }
 
+// 剧目信息
 type DramaInfo struct {
 	DramaId           int64      `json:"drama_id"`           // 剧目id。
 	CreateTime        int64      `json:"create_time"`        // 创建时间，时间戳。
 	Name              string     `json:"name"`               // 剧名。
+	Playwright        string     `json:"playwright"`         // 编剧
+	Producer          string     `json:"producer"`           // 制作方
+	ProductionLicense string     `json:"production_license"` // 备案号
 	CoverUrl          string     `json:"cover_url"`          // 封面图临时链接，根据提审时提交的cover_material_id转存得到。
 	MediaCount        int64      `json:"media_count"`        // 剧集数目
-	Producer          string     `json:"producer"`           // 制作方 。
-	Playwright        string     `json:"playwright"`         // 编剧
+	Recommendations   string     `json:"recommendations"`    // 推荐语
 	Description       string     `json:"description"`        // 描述
-	ProductionLicense string     `json:"production_license"` // 广播电视节目制作经营许可证。
+	PromotionPoster   string     `json:"promotion_poster"`
+	Status            int64      `json:"status"`
 	MediaList         []struct { // 审核状态。
 		MediaId int64 `json:"media_id"` // 媒资文件id。
 	} `json:"media_list"`
 	AuditDetail *AuditDetail `json:"audit_detail,omitempty"`
+	ActorList   *ActorList   `json:"actor_list,omitempty"`
 }
 
 // 获取剧目列表
-
 type GetDramaListResponse struct {
 	BaseResponse
 	DramaInfoList []*DramaInfo `json:"drama_info_list"`
@@ -115,4 +154,89 @@ type GetDramaListResponse struct {
 type GetDramaInfoResponse struct {
 	BaseResponse
 	DramaInfo *DramaInfo `json:"drama_info"`
+}
+
+// 查询剧目审核
+type SearchDramaAuditInfoResponse struct {
+	BaseResponse
+	AuditDetail *AuditDetail `json:"audit_detail"`
+}
+
+// 查询CDN
+type SearchCdnInfoResponse struct {
+	BaseResponse
+	DataInterval int64          `json:"data_interval"`
+	ItemList     []*CdnItemList `json:"item_list"`
+}
+
+type CdnItemList struct {
+	Time  int64 `json:"time"`
+	Value int64 `json:"value"`
+}
+
+// 查询CDN日志
+type SearchCdnLogDownLoadUrlResponse struct {
+	BaseResponse
+	DomesticCdnLogs []*DomesticCdnLogs `json:"domestic_cdn_logs"`
+	TotalCount      int64              `json:"total_count"`
+}
+
+type DomesticCdnLogs struct {
+	Date      string `json:"date"`
+	EndTime   int64  `json:"end_time"`
+	Name      string `json:"name"`
+	StartTime int64  `json:"start_time"`
+	Url       string `json:"url"`
+}
+
+// 流量包详情
+type ListPackageDetailResponse struct {
+	BaseResponse
+	PackageList []*PackageList `json:"package_list"`
+	TotalCount  int            `json:"total_count"`
+}
+
+type PackageList struct {
+	All       int64  `json:"all"`
+	EndTime   int64  `json:"end_time"`
+	IsDeleted int64  `json:"is_deleted"`
+	OrderId   int64  `json:"order_id"`
+	PackageId string `json:"package_id"`
+	StartTime int64  `json:"start_time"`
+	Status    int64  `json:"status"`
+	Used      int64  `json:"used"`
+}
+
+// 剧目授权
+type DramaAuthorizedResponse struct {
+	BaseResponse
+	Result []*DramaAuthorizedResult `json:"result"`
+}
+
+// 授权结果
+type DramaAuthorizedResult struct {
+	DramaId int64  `json:"drama_id"`
+	Errcode int64  `json:"errcode"`
+	Errmsg  string `json:"errmsg,omitempty"`
+}
+
+// 查询剧目授权
+type SearchDramaAuthorizedResponse struct {
+	BaseResponse
+	Objects    []*AuthorizedObjects `json:"objects"`
+	TotalCount int64                `json:"total_count"`
+}
+
+// 授权信息
+type AuthorizedObjects struct {
+	DramaId         int64  `json:"drama_id,omitempty"`
+	AuthorizedAppid string `json:"authorized_appid"`
+	AuthorizedTime  int64  `json:"authorized_time"`
+	AuthzExpireTime int64  `json:"authz_expire_time"`
+}
+
+// 查询账号授权
+type SearchAccountAuthorizedResponse struct {
+	BaseResponse
+	Objects []*AuthorizedObjects `json:"objects"`
 }
