879d45472634649c7537c08ce9015d62b7db962c
梁庆 <245374011@qq.com>
2026-02-04 18:42:26 +0800

    fix(wedrive): 更新分块上传完成响应结构
    - 移除upload_key字段
    - 添加fileid字段用于返回文件唯一标识

--- src/work/oa/wedrive/response/ResponseWeDriveFileUploadFinish.go
+++ src/work/oa/wedrive/response/ResponseWeDriveFileUploadFinish.go
@@ -5,5 +5,5 @@ import "github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/response"
 // ResponseWeDriveFileUploadFinish 分块上传完成响应
 type ResponseWeDriveFileUploadFinish struct {
 	response.ResponseWork
-	UploadKey string `json:"upload_key"` // 文件上传凭证。file_upload_init返回的upload_key
+	Fileid string `json:"fileid"` // 文件fileid
 }
