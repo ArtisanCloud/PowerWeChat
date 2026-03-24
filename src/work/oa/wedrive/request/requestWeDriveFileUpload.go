13b64625e9105cd3e10f6b409f3deb920da8a034
梁庆 <245374011@qq.com>
2026-03-24 17:12:06 +0800

    feat(wedrive): 更新微盘文件上传请求结构
    - 移除UserID字段
    - 添加SelectedTicket字段用于微盘和文件选择器jsapi
    - 更新SpaceID和FatherID字段注释说明
    - 更新FileName字段注释说明，添加字符限制提醒
    - 更新FileBase64Content字段注释，添加文件大小限制说明

--- src/work/oa/wedrive/request/requestWeDriveFileUpload.go
+++ src/work/oa/wedrive/request/requestWeDriveFileUpload.go
@@ -1,9 +1,9 @@
 package request
 
 type RequestWeDriveFileUpload struct {
-	UserID            string `json:"userid"`
-	SpaceID           string `json:"spaceid"`
-	FatherID          string `json:"fatherid"`
-	FileName          string `json:"file_name"`
-	FileBase64Content string `json:"file_base64_content"` // 文件内容base64（注意：只需要填入文件内容的Base64，不需要添加任何如：”data:application/x-javascript;base64” 的数据类型描述信息）
+	SpaceID           string `json:"spaceid"`             // 空间spaceid
+	FatherID          string `json:"fatherid"`            // 父目录fileid, 在根目录时为空间spaceid
+	SelectedTicket    string `json:"selected_ticket"`     // 微盘和文件选择器jsapi返回的selectedTicket。若填此参数，则不需要填spaceid/fatherid。
+	FileName          string `json:"file_name"`           // 文件名字（注意：文件名最多填255个字符, 英文算1个, 汉字算2个）
+	FileBase64Content string `json:"file_base64_content"` // 文件内容base64（注意：只需要填入文件内容的Base64，不需要添加任何如："data:application/x-javascript;base64" 的数据类型描述信息），文件大小上限为10M。大于10M文件，可使用文件分块上传接口
 }
